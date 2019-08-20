19dbb93fabf10bbab626ff10a2f768ef22efb270
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2019-08-20 10:50:54 +1000

    Added utility for parsing runner mode

---
+++ tests/unit/test_utils/test_beam_utils.py
@@ -0,0 +1,35 @@
+# test_beam_utils.py
+
+# Author : aarontillekeratne
+# Date : 2019-08-20
+
+# This file is part of aa-cloud-demo.
+
+# aa-cloud-demo is free software:
+# you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+
+# aa-cloud-demo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with aa-cloud-demo.  
+# If not, see <https://www.gnu.org/licenses/>.
+
+from src.utils.beam_utils import beam_runner_args_parser
+
+
+class TestBeamUtils:
+
+    def test_beam_runner_args_parser(self):
+        """
+        Tests that the beam runner args parser returns the correct format.
+        :return:
+        """
+        t_runner = 'direct'
+        expected = '--runner=DirectRunner'
+
+        assert beam_runner_args_parser(t_runner) == expected
